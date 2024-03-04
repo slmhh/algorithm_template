c7d1a342b310fba107d10c86c4feceaa06789aa8
slmhh <109951378+slmhh@users.noreply.github.com>
2024-03-04 19:24:12 +0800

    Update Kruskal算法-最小生成树.cpp

--- 搜索 图论/Kruskal算法-最小生成树.cpp
+++ 搜索 图论/Kruskal算法-最小生成树.cpp
@@ -1,29 +1,66 @@
 #include<bits/stdc++.h>
-#define debug(x) cout << "#x" << x << endl;
-#define all(x) (x).begin(),(x).end()
 using namespace std;
 
-//Å£¿Í  Ó¢Óï×÷ÎÄ 
+//æ— è´Ÿæƒè¾¹ 
 typedef long long ll;
-typedef pair<int,int> PII;
-const int N = 2e5 + 10;
-ll inf = 0x3f3f3f3f;
+typedef pair<ll,ll> PII;
+const int N = 1e6 + 10;
+
+struct edge{
+	ll next; 
+	ll to; 
+	ll weight;  
+};
+
+ll head[N],dist[N],n,m,start = 1,res = 0,cnt = 0;
+bool st[N];
+vector<edge> e;
+
+void add(int a,int b,int c){
+	e.push_back({head[a],b,c}); 
+	head[a] = e.size() - 1;
+} 
+
+void prim(){
+	priority_queue <PII,vector<PII>,greater<PII> > heap;
+	heap.push({0,start}); 
+	
+	memset(dist,0x3f3f3f3f,sizeof(dist)); 
+	dist[start] = 0;
+	 
+	while(heap.size()){
+		PII t = heap.top();
+		heap.pop();
+		
+		ll ver = t.second,d = t.first;
+		if(st[ver] == 1) continue;
+		st[ver] = 1;
+		res += d;
+		cnt++;
+		
+		for(int i = head[ver];i != -1;i = e[i].next){
+			ll j = e[i].to,k = e[i].weight;
+			if(dist[j] > k){
+				dist[j] = k;
+				heap.push({dist[j],j});
+			}
+		} 
+	}
+}
 
 int main(){
-	int T;
-	//cin >> T;
-	T = 1;
-	while(T--){
-		ll n,k,cnt = 0;
-        string word;
-        map<string,vector<ll> > w;
-        cin >> n >> k;
-        for(int i = 0;i < n;i++){
-            cin >> word;
-            cnt += w[word].end() - lower_bound(w[word].begin(),w[word].end(),i - k - 1);
-            w[word].push_back(i);
-        }
-        cout << cnt << endl;
-	}
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin >> n >> m;
+	memset(head,-1,sizeof(head));
+	while(m--){
+		ll x,y,z;
+		cin >> x >> y >> z;
+		add(x,y,z);
+		add(y,x,z);
+	}		
+	prim();
+	if(cnt == n) cout << res << "\n";
+	else cout << "orz\n";
 	return 0;
 }
+ 
